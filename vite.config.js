ac1965ccc2d0f2b64eb0d62e18d2f2e48a31c16e
Dhatri-Dintakurthi <dintakurthidhatri@gmail.com>
2025-11-17 08:57:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 👇 your repo name here – must match exactly
   base: '/hospital-frontend-cicd/',
   plugins: [react()],
 })
